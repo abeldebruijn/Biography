63fa5684805bc491188188b8a1d13c106a3caa04
Abel de Bruijn <abeldebruijn@hotmail.com>
2026-02-27 11:57:03 +0100

    added: srr is_touch_device

--- src/lib/is-touch-device.ts
+++ src/lib/is-touch-device.ts
@@ -1,22 +1,26 @@
 export function is_touch_device() {
+  if (typeof window === "undefined" || typeof document === "undefined") {
+    return false;
+  }
+
   try {
     const prefixes = ["-webkit-", "-moz-", "-o-", "-ms-"];
 
-    const mq = (query: string) => window.matchMedia(query).matches;
+    const mq = (query: string) =>
+      !!(window.matchMedia && window.matchMedia(query).matches);
 
     if (
       "ontouchstart" in window ||
-      // @ts-expect-error This is a valid check for older browsers that have a DocumentTouch interface
-      (typeof window.DocumentTouch !== "undefined" &&
-        // @ts-expect-error This is a valid check for older browsers that have a DocumentTouch interface
-        document instanceof window.DocumentTouch)
+      (typeof (window as any).DocumentTouch !== "undefined" &&
+        document instanceof (window as any).DocumentTouch)
     ) {
       return true;
     }
 
     return mq(["(", prefixes.join("touch-enabled),("), "heartz", ")"].join(""));
   } catch (e) {
-    console.error("(Touch detect failed)", e);
+    if (typeof console !== "undefined")
+      console.error("(Touch detect failed)", e);
     return false;
   }
 }
